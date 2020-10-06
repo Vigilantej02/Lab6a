9f6cc8032bebe7b62d3f7b0502ab782ca6338b4b
Vigilantej02 <j.vigilante97@gmail.com>
2020-10-06 21:56:09 +0000

    used a foreach loop rather than the god awful way i did it before

--- main.cs
+++ main.cs
@@ -19,18 +19,22 @@ class MainClass {
         }
     //sorting the farenheitTemperature array in DESCENDING order
     Array.Reverse(farenheitTemperature);
-    /*And this is where my problems start.
-    I ended up having to hardcode these.
-    I'm not quite sure what I'm doing wrong, but I'm almost certain its because I dont think i understand foreach loops all the way. 
-    Will fix when I find a solution */
+    
     Console.WriteLine("Here are the temperatures in farenheit");
+    foreach (int farenheit in farenheitTemperature)
+      {
+      Console.WriteLine(farenheit);
+      }
+     /*I'm gonna leave this horrendous mess in here, because this is the solution I came up with before figuring out how to use the foreach loop.
+
       Console.WriteLine(farenheitTemperature[0]);
       Console.WriteLine(farenheitTemperature[1]);
       Console.WriteLine(farenheitTemperature[2]); Console.WriteLine(farenheitTemperature[3]);Console.WriteLine(farenheitTemperature[4]); Console.WriteLine(farenheitTemperature[5]);Console.WriteLine(farenheitTemperature[6]);
+    */
+    //theres probably a better way to do this. 
       int sum = farenheitTemperature[0] + farenheitTemperature[1] + farenheitTemperature[2] + farenheitTemperature[3] + farenheitTemperature[4] + farenheitTemperature[5] + farenheitTemperature[6];
       Console.WriteLine("The sum of these temperatures in farenhit is " + sum);
       int avg = sum / 7;
       Console.WriteLine("The average temperture in farenheit for the next seven days is " + avg + " degrees.");
-      //Good god thats some ugly code. Mind the mess.
     }
     }
